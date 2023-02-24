ce1b97c98125431ae64ed04c31f6f9c92951085f
Adnaan Hussain <adnaan137@gmail.com>
2023-02-24 23:35:16 +0530

    First Commit on Hooks

--- src/components/useLocalStorage.js
+++ src/components/useLocalStorage.js
@@ -2,7 +2,14 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const useLocalStorage = (stateVar, value) => {
-  
+  const[ curState, setState] = useState(value);
+
+  updateStorage(stateVar,curState);
+  return [curState,setState];
 };
+
+const updateStorage = (key, value) => {
+    localStorage.setItem(key,value);
+}
 
 export default useLocalStorage;
